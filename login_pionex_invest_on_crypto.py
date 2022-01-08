1e83cf746639a7d787599c0e3fa8060263d8f335
anichelino <mottola.mottola@gmail.com>
2022-01-08 14:21:05 +0100

    Update login_pionex_invest_on_crypto.py

--- login_pionex_invest_on_crypto.py
+++ login_pionex_invest_on_crypto.py
@@ -39,9 +39,9 @@ class Login_Pionex_Invest_On_Crypto:
       # enable trace level for debugging 
       options.log.level = "trace"
 
-      options.add_argument("-remote-debugging-port=9224")
+      options.add_argument("-disable-dev-shm-usage")
       options.add_argument("-headless")
-      options.add_argument("-disable-gpu")
+      #options.add_argument("-disable-gpu")
       options.add_argument("-no-sandbox")
 
       binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
