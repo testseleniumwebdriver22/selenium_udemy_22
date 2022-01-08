58728b9989bc547ee3c557069b6336a364106dfc
anichelino <mottola.mottola@gmail.com>
2022-01-08 19:59:06 +0100

    Update login_pionex_invest_on_crypto.py

--- login_pionex_invest_on_crypto.py
+++ login_pionex_invest_on_crypto.py
@@ -194,7 +194,7 @@ class Login_Pionex_Invest_On_Crypto:
     class_name="ant-modal-confirm-content"
     
     time.sleep(4)
-    self.driver.find_element_by_xpath("//*[contains(@class,'ant-btn')]")
+    self.driver.find_element_by_xpath("//*[contains(@class,'ant-btn2')]")
     print("cancel button clicked")
     # try:
     #              myElem = WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.CLASS_NAME,class_name)))
