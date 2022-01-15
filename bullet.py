3cb50bb599b88b755e7b08dd209a3359ad2dde95
anichelino <mottola.mottola@gmail.com>
2022-01-15 11:25:24 +0100

    credentials removed

--- bullet.py
+++ bullet.py
@@ -5,8 +5,8 @@ class Bullet:
        
  def __init__(self,string_to_send):
 
-    token = "2054727581:AAH_RVxNd7F2fxCpxGbS7gw6CPp23ED-9YE"
-    chat_id = "1761157621"
+    token = "insert your token"
+    chat_id = "insert your chat id"
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text="+string_to_send 
     #url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=bau bau" 
     results = requests.get(url_req)
